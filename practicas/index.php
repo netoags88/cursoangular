003f2c4ff9908883e9335def90daa141618ddbdb
neto <neto@neto-lap>
2015-12-29 14:38:22 -0600

    el index

--- practicas/index.php
+++ practicas/index.php
@@ -4,12 +4,15 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
+<html lang="es">
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="stylesheet" href="bower_components/angular/angular-csp.css">
     </head>
-    <body>
+    <body ng-app="">
         <h1>Curso angular</h1>
+        <h1>{{"Hola Mundo"}}</h1>
+        <script src="bower_components/angular/angular.js"></script>
     </body>
 </html>
