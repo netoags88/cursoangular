e854764bf6e455155e045cbafe5ba057ebabcc33
neto <neto@neto-lap>
2015-12-29 14:22:07 -0600

    Ejemplo de expresiones con strings

---
+++ practicas/expresiones/holamundo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Hola Mundo</title>
+        <link rel="stylesheet" href="../bower_components/angular/angular-csp.css">
+    </head>
+    <body ng-app="">
+        <header>Curso angular</header>
+        <h1>{{"Hola Mundo"}}</h1>
+        <script src="../bower_components/angular/angular.js"></script>
+    </body>
+</html>
